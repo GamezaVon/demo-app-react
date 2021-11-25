7515a40350d472b0593d3fe836082a508c652785
Daniel Gamez <acocolito@gmail.com>
2021-11-24 22:26:02 -0700

    Removed eslint buggy warnings & errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 
 function App() {
